d1c9c75739f508a9a7e73403f8b68d04149b8c4b
BVezilic <banex93@gmail.com>
2016-05-17 19:34:50 +0200

    Deactive

--- AgentiWeb/WebContent/js/agentListController.js
+++ AgentiWeb/WebContent/js/agentListController.js
@@ -1,7 +1,13 @@
 var app = angular.module('MyApp', []);
 app.controller('AgentListCtrl', function($scope, $http) {
+	$scope.switch = true;
 	$scope.AgentType = 'Ping';
     $scope.activate = function() {    	
         $http.put("http://localhost:8080/AgentiWeb/rest/agentskiCentar/agents/running/"+$scope.AgentType+"/"+$scope.pingName);
+        $scope.switch = false;
+    };
+    $scope.deactivate = function() {    	
+        $http.delete("http://localhost:8080/AgentiWeb/rest/agentskiCentar/agents/running/"+$scope.pingName);
+        $scope.switch = true;
     };
 });
