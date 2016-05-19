e3896d0824cdce8c7302374f1b3b1eeefe2680d0
BVezilic <banex93@gmail.com>
2016-05-19 23:08:39 +0200

    test

--- AgentiWeb/WebContent/js/agentListController.js
+++ AgentiWeb/WebContent/js/agentListController.js
@@ -1,12 +1,15 @@
 var app = angular.module('MyApp', []);
 app.controller('AgentListCtrl', function($scope, $http) {
 	$scope.switch = true;
-	$scope.AgentType = 'Ping';
-    $scope.activate = function() {    	
-        $http.put("http://localhost:8080/AgentiWeb/rest/agentskiCentar/agents/running/"+$scope.AgentType+"/"+$scope.pingName);
-        $scope.switch = false;
+	$scope.supportedAgents = ['Ping','Pong','Map'];
+	//$http.get("http://localhost:8080/AgentiWeb/rest/agentskiCentar/agents").then(function (response) {$scope.supportedAgents = response.data;});
+    $scope.activate = function(agentType, agentName) {   
+    	alert(agentName +' ' + agentType);
+        //$http.put("http://localhost:8080/AgentiWeb/rest/agentskiCentar/agents/running/"+$scope.AgentType+"/"+$scope.pingName);
+        //$scope.switch = false;
+    	//dodao sam komentar
     };
-    $scope.deactivate = function() {    	
+    $scope.deactivate = function(agentType, agentNames) {    	
         $http.delete("http://localhost:8080/AgentiWeb/rest/agentskiCentar/agents/running/"+$scope.pingName);
         $scope.switch = true;
     };
